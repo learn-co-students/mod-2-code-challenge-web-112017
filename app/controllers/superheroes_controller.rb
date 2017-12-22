532071fe070f399803f7ac5bf7ed15f2a0a3f81c
github username <github email address>
2017-12-22 11:02:54 -0500

    initial commit

--- app/controllers/superheroes_controller.rb
+++ app/controllers/superheroes_controller.rb
@@ -2,4 +2,32 @@ class SuperheroesController < ApplicationController
   def index
     @superheroes = Superhero.all
   end
+
+  def show
+    @superhero = Superhero.find(params[:id])
+    byebug
+  end
+
+  def new
+    @superhero = Superhero.new
+
+    @superpowers = Superpower.all
+  end
+
+  def create
+    @superhero = Superhero.new(superhero_params)
+
+    if @superhero.valid?
+      @superhero.save
+      redirect_to superhero_path(@superhero)
+    else
+      flash[:errors]
+      redirect_to new_superhero_path
+    end
+  end
+
+  def superhero_params
+    params.require(:superhero).permit(:name, :super_name, :superpower)
+  end
+
 end
