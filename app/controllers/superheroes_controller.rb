8ba7b8e885a8b2bf625f7407e7a6fa9a2b801063
jerrybennett <jerrysbusiness@gmail.com>
2017-12-22 11:03:05 -0500

    my commit

--- app/controllers/superheroes_controller.rb
+++ app/controllers/superheroes_controller.rb
@@ -2,4 +2,24 @@ class SuperheroesController < ApplicationController
   def index
     @superheroes = Superhero.all
   end
+
+  def show
+    @superhero = Superhero.find(params[:id])
+  end
+
+  def create
+    @superhero = Superhero.create(hero_params)
+    if @superhero.create
+      @superhero.save
+      redirect_to superhero_path(@superhero)
+    else
+
+    end
+  end
+
+  private
+
+  def hero_params
+    params.require(:superhero).permit(:name, :super_name, :superpower_id)
+  end
 end
