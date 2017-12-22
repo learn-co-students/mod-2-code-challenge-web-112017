789683a4c8a62b4a218605730038a976fffbe54e
L-A-Davis <lindsay.amanda.davis@gmail.com>
2017-12-22 11:02:08 -0500

    complete code challenge

--- app/controllers/superheroes_controller.rb
+++ app/controllers/superheroes_controller.rb
@@ -1,5 +1,39 @@
 class SuperheroesController < ApplicationController
   def index
     @superheroes = Superhero.all
+    if params[:search]
+      @superheroes = Superhero.search(params[:search])
+    else
+      @superheroes = Superhero.all
+    end
   end
+
+  def show
+    @superhero = Superhero.find(params[:id])
+  end
+
+  def new
+    @superhero = Superhero.new
+    @superpowers = Superpower.all
+  end
+
+  def create
+    @superhero = Superhero.new(superhero_params)
+    if @superhero.valid?
+      @superhero.save
+      redirect_to @superhero
+    else
+      flash[:error] = @superhero.errors.full_messages
+      render :new
+    end
+  end
+
+private
+
+def superhero_params
+  params.require(:superhero).permit(:name, :super_name, :superpower_id)
+end
+
+
+
 end
--- app/models/superhero.rb
+++ app/models/superhero.rb
@@ -1,2 +1,12 @@
 class Superhero < ApplicationRecord
+  belongs_to :superpower
+  validates :super_name, uniqueness: true
+
+def self.search(search)
+  if @superpower = Superpower.find_by(name: search)
+  where("superpower_id LIKE ?", @superpower.id)
+else
+end
+end
+
 end
