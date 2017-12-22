78de4c30d4b6e19b6d1ca7cc6b0519da82cac855
ddbrennan <ddbren@gmail.com>
2017-12-22 10:23:17 -0500

    superhero superpower relations created

--- app/models/superhero.rb
+++ app/models/superhero.rb
@@ -1,2 +1,13 @@
 class Superhero < ApplicationRecord
+  validates :super_name, uniqueness: true
+
+  belongs_to :superpower
+
+  def self.search(q)
+    if !q.to_s.empty?
+      self.all.select {|hero| hero.superpower.name == q.downcase}
+    else
+      self.all
+    end
+  end
 end
