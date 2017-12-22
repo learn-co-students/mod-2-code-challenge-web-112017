629ef5f9a4300bd9f153fa47de510d797f4f93e4
eal6gf <elum1234@gmail.com>
2017-12-22 11:01:52 -0500

    i done goofed

--- app/controllers/superheroes_controller.rb
+++ app/controllers/superheroes_controller.rb
@@ -1,5 +1,47 @@
 class SuperheroesController < ApplicationController
+
+  def show
+    @superhero = Superhero.find(params[:id])
+  end
+
+  def new
+    @superhero = Superhero.new
+    @superpowers = Superpower.all
+  end
+
+  def create
+    @superhero = Superhero.new(superhero_params)
+    byebug
+    if @superhero.save
+      redirect_to @superhero
+    else
+      redirect_to new_superhero_path
+    end
+  end
+
   def index
     @superheroes = Superhero.all
+    if params[:search]
+      @superheroes = Superhero.search(params[:search]).order("created_at DESC")
+    else
+      @superheroes = Superhero.all.order('created_at DESC')
+    end
+  end
+
+  def destroy
+
+  end
+
+  def update
+
+  end
+
+  def edit
+
+  end
+
+  private
+  def superhero_params
+    params.require(:superhero).permit(:super_name, :name, superpowers_attributes: [:id] )
   end
 end
--- app/models/superhero.rb
+++ app/models/superhero.rb
@@ -1,2 +1,10 @@
 class Superhero < ApplicationRecord
+  belongs_to :superpower
+  accepts_nested_attributes_for :superpower
+
+  def self.search(search)
+    where("name LIKE ?", "%#{search}%")
+    where("content LIKE ?", "%#{search}%")
+  end
+
 end
