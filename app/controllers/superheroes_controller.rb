a1d88fdf66f0b44736dd369d8f07d534c022f9df
github username <github email address>
2017-12-22 11:04:16 -0500

    test submission

--- app/controllers/superheroes_controller.rb
+++ app/controllers/superheroes_controller.rb
@@ -1,5 +1,19 @@
 class SuperheroesController < ApplicationController
   def index
     @superheroes = Superhero.all
+    # if params[:search]
+    #   @superheroes = Superhero.search(params[:search]).order("created_at DESC")
+    # else
+    #   @superheroes = Superhero.all.order('created_at DESC')
+    # end
+  end
+
+  def new
+    @superhero = Superhero.new
+    @superpowers = Superpower.all
+  end
+
+  def show
+    @superhero = Superhero.find(params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   resources :superpowers, only: [:index]
-  resources :superheroes, only: [:index]
+  resources :superheroes, only: [:index, :show, :new]
 
+  get "/superheros/:id", to: "superhero#show", as: "superheroes_path"
+  get "/superpowers/:id", to: "superpower#show", as: "superpowers_path"
 end
