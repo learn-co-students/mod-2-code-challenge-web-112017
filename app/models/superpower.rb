9d599785b86e906d6f2bd4279f4712a9ff4e02a9
KellyStamps <kellydsample@gmail.com>
2017-12-22 11:00:41 -0500

    mod 2 complete except search bar functions corrected

--- app/models/superpower.rb
+++ app/models/superpower.rb
@@ -1,2 +1,9 @@
 class Superpower < ApplicationRecord
+  has_many :superheroes
+
+  def self.search(search)
+    where("name LIKE ?", "%#{search}%")
+  end
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
-  resources :superpowers, only: [:index]
-  resources :superheroes, only: [:index]
+  resources :superpowers, only: [:index, :show]
+  resources :superheroes, only: [:index, :show, :new, :create]
+  # resources :superheroes do
+  #    collection do
+  #     get :search
+  #   end
+  # end
+
 
 end
