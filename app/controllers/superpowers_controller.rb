b56dbc07aae1f6284e9e25fe74a86ad5db81ceb8
github username <github email address>
2017-12-22 11:02:11 -0500

    not completed

--- app/controllers/superpowers_controller.rb
+++ app/controllers/superpowers_controller.rb
@@ -2,4 +2,31 @@ class SuperpowersController < ApplicationController
   def index
     @superpowers = Superpower.all
   end
+
+  def show
+    @superpower = Superpower.find(params[:id])
+  end
+
+  def edit
+    @superpower = Superpower.new
+  end
+
+  def create
+    @superpower = Superpower.new(superpower_params)
+
+    if @superpower.valid?
+      @superpower.save
+      redirect_to superpower_path(@superpower)
+    else
+      flash[:error] = @superpower.errors.full_messages
+      redirect_to new_superpower
+    end
+  end
+
+
+  private
+
+  def superpower_params
+    params.require(:superpower).permit(:name, :description)
+  end
 end
---
+++ app/models/add_super_power_to_super_hero.rb
@@ -0,0 +1,3 @@
+class AddSuperPowerToSuperHero < ApplicationRecord
+  belongs_to :super_power
+end
