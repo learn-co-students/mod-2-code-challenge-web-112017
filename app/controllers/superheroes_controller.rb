0ed693dc1c4afc0ce8ebf8ea3756a67981b126cb
nspp99b <chromepanthers@gmail.com>
2017-12-22 11:02:15 -0500

    configured controllers, models, views, without complete implementation of search

--- app/controllers/superheroes_controller.rb
+++ app/controllers/superheroes_controller.rb
@@ -1,5 +1,39 @@
 class SuperheroesController < ApplicationController
+
   def index
-    @superheroes = Superhero.all
+      @search = Superhero.new(params[:superhero])
+    # if @search
+    #   @superheroes = Superhero.find_by_name(@search.name)
+    # else
+      @superheroes = Superhero.all
   end
+    # @search = Animal.new(params[:animal])
+    # @animals = Animal.by_color(@search.color)
+
+  def new
+    @superhero = Superhero.new
+    @superpowers = Superpower.all
+  end
+
+  def create
+    @superhero = Superhero.create(superhero_params)
+    if @superhero.valid?
+      @superhero.save
+      redirect_to superhero_path(@superhero)
+    else
+      flash[:error] = @superhero.errors.full_messages  #flash[:error] is an array of individual ‘error’ messages)
+      redirect_to new_superhero_path
+    end
+  end
+
+  def show
+    @superhero = Superhero.find(params[:id])
+  end
+
+  private
+
+  def superhero_params
+    params.require(:superhero).permit(:name, :super_name, :superpower_id, :term)
+  end
+
 end
--- app/controllers/superpowers_controller.rb
+++ app/controllers/superpowers_controller.rb
@@ -1,5 +1,17 @@
 class SuperpowersController < ApplicationController
+
   def index
     @superpowers = Superpower.all
   end
+
+  def new
+  end
+
+  def create
+  end
+
+  def show
+    @superpower = Superpower.find(params[:id])
+  end
+
 end
